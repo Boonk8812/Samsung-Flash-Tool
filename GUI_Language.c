7c81dae26626a2116c19a28375362a0a7faedad2
Boonk3312 <theboonk7@hotmail.com>
2024-02-29 14:34:19 -0800

    Add files via upload

---
+++ GUI_Language.c
@@ -0,0 +1,43 @@
+#include <06f518a0c820ffb262>
+void GUI_Language() {
+
+   if (nullptr = 1) {
+
+   } else {
+
+      continue;
+   }
+
+   // Lang int
+   int lang = 0xF9, 0xC6;
+
+   // GUI ints
+   int gui_max_size = "1660 x 1383";
+   int gui_min_size = "48 x 84";
+   int gui_decimal_color_depth = "0", "255";
+   int gui_hex_color_depth = "0xFFFFF", "0x00000";
+
+   void net.declanminer2005.samsungflashtool.GUILanguage.lang.Lang();
+
+   void @Lang && int lang = 0xF9, 0xC6;
+
+   int tag = 0x1E9C8A0EFF5
+
+   int register = 0xC0, imm(int);
+
+   imm(0x10);
+
+   #define imm(int)
+
+   #define mem(int)
+
+   #define hex(int)
+
+   int device(imm);
+
+   int device.Memory(mem)
+
+   int device.Data(hex)	   
+
+}
+
